a62e4f1cf2c99150d345917c290f13e5a48ab92e
Hazelnoot <acomputerdog@gmail.com>
2024-11-14 19:32:08 -0500

    ignore `isNSFW` for pure renotes

--- packages/backend/src/core/NoteEditService.ts
+++ packages/backend/src/core/NoteEditService.ts
@@ -142,6 +142,8 @@ type Option = {
 	editcount?: boolean | null;
 };
 
+type PureRenoteOption = Option & { renote: MiNote } & ({ text?: null } | { cw?: null } | { reply?: null } | { poll?: null } | { files?: null | [] });
+
 @Injectable()
 export class NoteEditService implements OnApplicationShutdown {
 	#shutdownController = new AbortController();
@@ -442,7 +444,7 @@ export class NoteEditService implements OnApplicationShutdown {
 
 		if (user.host && !data.cw) {
 			await this.federatedInstanceService.fetch(user.host).then(async i => {
-				if (i.isNSFW) {
+				if (i.isNSFW && !this.isPureRenote(data)) {
 					data.cw = 'Instance is marked as NSFW';
 				}
 			});
@@ -785,6 +787,11 @@ export class NoteEditService implements OnApplicationShutdown {
 
 		// Register to search database
 		if (!user.noindex) this.index(note);
+	}
+
+	@bindThis
+	private isPureRenote(note: Option): note is PureRenoteOption {
+		return this.isRenote(note) && !this.isQuote(note);
 	}
 
 	@bindThis

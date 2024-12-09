116a14720285707f2b884ccf6d39e224b04ec390
Marie <github@yuugi.dev>
2024-12-09 05:00:20 +0000

    remove comma added by vscode prettifer

--- packages/frontend/src/components/MkPostForm.vue
+++ packages/frontend/src/components/MkPostForm.vue
@@ -821,7 +821,7 @@ async function post(ev?: MouseEvent) {
 		const filesData = toRaw(files.value);
 
 		const isMissingAltText = filesData.filter(
-			file => file.type.startsWith('image/') || file.type.startsWith('video/') || file.type.startsWith('audio/'),
+			file => file.type.startsWith('image/') || file.type.startsWith('video/') || file.type.startsWith('audio/')
 		).some(file => !file.comment);
 
 		if (isMissingAltText) {

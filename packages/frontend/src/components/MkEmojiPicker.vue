794c7f9a1a8cd3b8a201c3aea86cee00c567c932
HellhoundSoftware <keith@keithhacks.cyou>
2025-01-15 21:04:35 -0500

    Change let to const

--- packages/frontend/src/components/MkEmojiPicker.vue
+++ packages/frontend/src/components/MkEmojiPicker.vue
@@ -186,7 +186,7 @@ function parseAndMergeCategories(input: string, root: CustomEmojiFolderTree): Cu
 	const parts = input.split('/').map(p => p.trim());
 	let currentNode: CustomEmojiFolderTree = root;
 
-	let currentPath = [];
+	const currentPath = [];
 	for (const part of parts) {
 		currentPath.push(part);
 		let existingNode = currentNode.children.find((node) => node.value === part);

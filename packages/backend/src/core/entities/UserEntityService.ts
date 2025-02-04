baff0605e0304784d8096ad99df7bf167fe83132
Hazelnoot <acomputerdog@gmail.com>
2025-02-04 09:16:35 -0500

    fix import order in UserEntityService.ts

--- packages/backend/src/core/entities/UserEntityService.ts
+++ packages/backend/src/core/entities/UserEntityService.ts
@@ -49,11 +49,11 @@ import { IdService } from '@/core/IdService.js';
 import type { AnnouncementService } from '@/core/AnnouncementService.js';
 import type { CustomEmojiService } from '@/core/CustomEmojiService.js';
 import { AvatarDecorationService } from '@/core/AvatarDecorationService.js';
+import { isSystemAccount } from '@/misc/is-system-account.js';
 import type { OnModuleInit } from '@nestjs/common';
 import type { NoteEntityService } from './NoteEntityService.js';
 import type { DriveFileEntityService } from './DriveFileEntityService.js';
 import type { PageEntityService } from './PageEntityService.js';
-import { isSystemAccount } from '@/misc/is-system-account.js';
 
 const Ajv = _Ajv.default;
 const ajv = new Ajv();

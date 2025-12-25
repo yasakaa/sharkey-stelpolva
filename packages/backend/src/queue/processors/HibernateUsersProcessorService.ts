27b28cf9d50e795e8bdb4d9e5e48721e7931e714
Yasaka <007viz143@gmail.com>
2025-12-25 20:54:38 +0900

    fix: オンライン判定バグに伴う休眠処理の一時無効化

--- packages/backend/src/queue/processors/HibernateUsersProcessorService.ts
+++ packages/backend/src/queue/processors/HibernateUsersProcessorService.ts
@@ -3,16 +3,16 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { Inject, Injectable } from '@nestjs/common';
-import { In, LessThan } from 'typeorm';
-import { QueueLoggerService } from '@/queue/QueueLoggerService.js';
-import type Logger from '@/logger.js';
-import { bindThis } from '@/decorators.js';
-import { renderInlineError } from '@/misc/render-inline-error.js';
-import { CacheService } from '@/core/CacheService.js';
-import { TimeService } from '@/global/TimeService.js';
-import type { FollowingsRepository, UsersRepository } from '@/models/_.js';
-import { DI } from '@/di-symbols.js';
+import { Inject, Injectable } from "@nestjs/common";
+import { In, LessThan } from "typeorm";
+import { QueueLoggerService } from "@/queue/QueueLoggerService.js";
+import type Logger from "@/logger.js";
+import { bindThis } from "@/decorators.js";
+import { renderInlineError } from "@/misc/render-inline-error.js";
+import { CacheService } from "@/core/CacheService.js";
+import { TimeService } from "@/global/TimeService.js";
+import type { FollowingsRepository, UsersRepository } from "@/models/_.js";
+import { DI } from "@/di-symbols.js";
 
 @Injectable()
 export class HibernateUsersProcessorService {
@@ -30,44 +30,50 @@ export class HibernateUsersProcessorService {
 
 		queueLoggerService: QueueLoggerService,
 	) {
-		this.logger = queueLoggerService.logger.createSubLogger('hibernate-users');
+		this.logger = queueLoggerService.logger.createSubLogger("hibernate-users");
 	}
 
 	@bindThis
 	public async process() {
-		try {
-			let totalHibernated = 0;
+		// エラー対応のため休眠阻止
+		this.logger.warn(
+			"Hibernation process is TEMPORARILY DISABLED due to online status bug.",
+		);
+		return;
 
-			// Any users last active *before* this date should be hibernated
-			const hibernationThreshold = new Date(this.timeService.now - (1000 * 60 * 60 * 24 * 50));
+		// try {
+		// 	let totalHibernated = 0;
 
-			// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-			while (true) {
-				// Work in batches of 100
-				const page = await this.usersRepository.find({
-					where: { isHibernated: false, lastActiveDate: LessThan(hibernationThreshold) },
-					select: { id: true },
-					take: 100,
-				}) as { id: string }[];
-				const ids = page.map(u => u.id);
+		// 	// Any users last active *before* this date should be hibernated
+		// 	const hibernationThreshold = new Date(this.timeService.now - (1000 * 60 * 60 * 24 * 50));
 
-				// Stop when we get them all
-				if (ids.length < 1) break;
+		// 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+		// 	while (true) {
+		// 		// Work in batches of 100
+		// 		const page = await this.usersRepository.find({
+		// 			where: { isHibernated: false, lastActiveDate: LessThan(hibernationThreshold) },
+		// 			select: { id: true },
+		// 			take: 100,
+		// 		}) as { id: string }[];
+		// 		const ids = page.map(u => u.id);
 
-				await this.usersRepository.update({ id: In(ids) }, { isHibernated: true });
-				await this.followingsRepository.update({ followerId: In(ids) }, { isFollowerHibernated: true });
-				await this.cacheService.hibernatedUserCache.refreshMany(ids);
+		// 		// Stop when we get them all
+		// 		if (ids.length < 1) break;
 
-				totalHibernated += ids.length;
-			}
+		// 		await this.usersRepository.update({ id: In(ids) }, { isHibernated: true });
+		// 		await this.followingsRepository.update({ followerId: In(ids) }, { isFollowerHibernated: true });
+		// 		await this.cacheService.hibernatedUserCache.refreshMany(ids);
 
-			if (totalHibernated > 0) {
-				this.logger.info(`Hibernated ${totalHibernated} inactive users`);
-			} else {
-				this.logger.debug('Skipping hibernation: nothing to do');
-			}
-		} catch (err) {
-			this.logger.error(`Error hibernating users: ${renderInlineError(err)}`);
-		}
+		// 		totalHibernated += ids.length;
+		// 	}
+
+		// 	if (totalHibernated > 0) {
+		// 		this.logger.info(`Hibernated ${totalHibernated} inactive users`);
+		// 	} else {
+		// 		this.logger.debug('Skipping hibernation: nothing to do');
+		// 	}
+		// } catch (err) {
+		// 	this.logger.error(`Error hibernating users: ${renderInlineError(err)}`);
+		// }
 	}
 }
